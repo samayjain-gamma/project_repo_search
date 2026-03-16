8e2730624c537b68355e835faaad2fb061ae71ef
Samay Jain <samay.jain@gammaedge.io>
2026-03-16 15:26:38 +0530

    tool_registry added

--- src/mcp_server/tools/github_tools.py
+++ src/mcp_server/tools/github_tools.py
@@ -8,13 +8,11 @@ def search_repository(query: str, top_k: int = 3):
     params = {"q": query, "sort": "stars", "order": "desc", "per_page": top_k}
 
     response = requests.get(GITHUB_SEARCH_URL, params=params)
-    print(response)
 
     if response.status_code != 200:
         raise Exception(f"GitHub API error: {response.status_code}")
 
     data = response.json()
-    print(data)
     repositories = []
 
     for repo in data.get("items", [])[:top_k]:
---
+++ src/mcp_server/tools/tool_registry.py
@@ -0,0 +1,36 @@
+from typing import Callable, Dict, List
+
+from src.core.exception import CustomException
+from src.core.logger import logger
+
+from .github_tools import search_repository
+
+
+class ToolRegistry:
+
+    def __init__(self):
+        self._tools: Dict[str, Callable] = {}
+
+    def register_tool(self, name: str, tool: Callable):
+        self._tools[name] = tool
+
+    def get_tool(self, name: str) -> Callable:
+        if name not in self._tools:
+            logger.info("Tool not in tools")
+            return
+        return self._tools[name]
+
+    def list_tools(self) -> List:
+        return list(self._tools)
+
+
+registry = ToolRegistry()
+
+registry.register_tool("search_repository", search_repository)
+
+if __name__ == "__main__":
+    github_tool = registry.get_tool("search_repository")
+    tools = registry.list_tools()
+    print(tools)
+    repos = github_tool("RAG")
+    print(repos)
